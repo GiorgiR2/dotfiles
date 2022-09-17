2662bd55af9dae4b6986259f04df69e0f69c3873
Giorgi Rakviashvili <rakviashvili.giorgi@gmail.com>
2022-09-18 01:35:50 +0400

    add packages

--- packages.sh
+++ packages.sh
@@ -1,5 +1,14 @@
 
-sudo apt install i3 i3status dmenu git ranger zsh neofetch neovim curl tmux kitty compton youtube-dl -y
+sudo apt update
+
+sudo apt install i3 i3status dmenu compton -y
+sudo apt install git zsh neofetch neovim curl tmux kitty youtube-dl -y
+sudo apt install ranger urlscan -y
+
+sudo apt install apt-transport-https -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+sudo apt install brave-browser-beta -y
 
 # need to PlugInstall
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
